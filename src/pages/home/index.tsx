65b79225b72b01674f403a589d4c036e2c398198
Pedro Henrique <pedro.henrique.martins404@gmail.com>
2024-03-29 17:14:57 -0300

    feat: :seedling: Create Style Css Home

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -26,21 +26,21 @@ export function Home() {
 
                 <tbody id='tbody'>
                     <tr className={styles.tr}>
-                        <td className={styles.tdLabel}>
-                            <Link to="/detail/btc">
-                                <span className={styles.link}>Bitcoin</span> | BTC
+                        <td className={styles.tdLabel} data-label="Moeda">
+                            <Link className={styles.link} to="/detail/btc">
+                                <span>Bitcoin</span> | BTC
                             </Link>
                         </td>
 
-                        <td className={styles.tdLabel}>
+                        <td className={styles.tdLabel} data-label="Mercado">
                             R$ 19253
                         </td>
 
-                        <td className={styles.tdLabel}>
+                        <td className={styles.tdLabel} data-label="Preço">
                             R$ 40.982
                         </td>
 
-                        <td className={styles.tdProfit}>
+                        <td className={styles.tdProfit} data-label="Volume">
                             <span>-5.3</span>
                         </td>
                     </tr>
