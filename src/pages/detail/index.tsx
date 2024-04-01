f723afad4f54b90a9f7e8c0cfefa840435c817ec
Pedro Henrique <pedro.henrique.martins404@gmail.com>
2024-04-01 16:05:29 -0300

    feat: :seedling: Start Detail Page

--- src/pages/detail/index.tsx
+++ src/pages/detail/index.tsx
@@ -1,8 +1,19 @@
+import { useEffect, useState } from 'react';
+import style from './detail.module.css';
+import { useParams } from 'react-router-dom';
 
 export function Detail() {
+    const { cripto } = useParams();
+    
+    useEffect(() => {
+        function getData() {
+            fetch(``)
+        }
+    })
+
     return(
         <div>
-            <h1>Pagina Detalhes</h1>
+            <h1>Pagina Detalhes {cripto}</h1>
         </div>
     )
 }
