f585917af9af02f9d2bc160eb113da1d7dce2794
Pedro Henrique <pedro.henrique.martins404@gmail.com>
2024-04-02 13:05:16 -0300

    feat: :seedling: Consult API Local for Detail Cripto

--- src/pages/detail/index.tsx
+++ src/pages/detail/index.tsx
@@ -1,19 +1,68 @@
 import { useEffect, useState } from 'react';
-import style from './detail.module.css';
+import styles from './detail.module.css';
 import { useParams } from 'react-router-dom';
+
+interface CoinProp {
+    name: string;
+    delta_24h: string;
+    show_symbol: string | null;
+    rank: string;
+    price: string;
+    symbol: string;
+    volume_24h: string;
+    market_cap: string;
+    formattedPrice: string;
+    formattedMarket: string;
+}
+
+interface DataProp {
+    0: CoinProp;
+}
 
 export function Detail() {
     const { cripto } = useParams();
+    const [detail, setDetail] = useState<CoinProp>()
+    const [loading, setLoading] = useState(true)
     
     useEffect(() => {
         function getData() {
-            fetch(``)
+            fetch(`http://localhost:4000/coins?symbol=${cripto}`)
+            .then(response => response.json())
+            .then((data: DataProp) => {
+
+                let price = Intl.NumberFormat('pt-BR', {
+                    style: 'currency',
+                    currency: 'BRL'
+                })
+                
+                const resultData = {
+                    ...data[0],
+                    formattedPrice: price.format(Number(data[0].price)),
+                    formattedMarket: price.format(Number(data[0].market_cap)),
+                }
+
+                // console.log(resultData)
+                setDetail(resultData);
+                setLoading(false);
+            })
+            
         }
-    })
+
+        getData()
+    }, [cripto])
+
+    if(loading) {
+        return(
+            <div className={styles.container}>
+                <h4 className={styles.center}>Carregando informações...</h4>
+            </div>
+        )
+    }
 
     return(
-        <div>
-            <h1>Pagina Detalhes {cripto}</h1>
+        <div className={styles.container}>
+            <h1 className={styles.center}>{detail?.name}</h1>
+            <p className={styles.center}>{detail?.symbol}</p>
         </div>
     )
 }
