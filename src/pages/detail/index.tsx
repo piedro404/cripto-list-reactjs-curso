7faaf43e3a8c779ca66bdc15809f39a602fc3bec
Pedro Henrique <pedro.henrique.martins404@gmail.com>
2024-04-02 13:20:18 -0300

    feat: :art: Detail CSS

--- src/pages/detail/index.tsx
+++ src/pages/detail/index.tsx
@@ -39,6 +39,7 @@ export function Detail() {
                     ...data[0],
                     formattedPrice: price.format(Number(data[0].price)),
                     formattedMarket: price.format(Number(data[0].market_cap)),
+                    delta_24h: data[0].delta_24h.replace(',', '.')
                 }
 
                 // console.log(resultData)
@@ -63,6 +64,22 @@ export function Detail() {
         <div className={styles.container}>
             <h1 className={styles.center}>{detail?.name}</h1>
             <p className={styles.center}>{detail?.symbol}</p>
+
+            <section className={styles.content}>
+                <p>
+                    <strong>Rank:</strong> {detail?.rank}°
+                </p>
+                <p>
+                    <strong>Preço:</strong> {detail?.formattedPrice}
+                </p>
+                <p>
+                    <strong>Valor de Mercado:</strong> {detail?.formattedMarket}
+                </p>
+                <p>
+                    <strong>Delta 24h:</strong> 
+                    <span className={Number(detail?.delta_24h) >= 0 ? styles.profit : styles.loss}>{detail?.delta_24h}</span>
+                </p>
+            </section>
         </div>
     )
 }
